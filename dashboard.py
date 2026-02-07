c2d5e76263af4608428a87325318d9d88fe6503c
nuwaira-batool <nuwaira.batool@icloud.com>
2026-02-07 19:39:12 +0500

    added a test main

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+import json
+import pandas as pd
+from loadData import load_data
+from cleanData import clean_data
+from filterData import filter_data
+from computeData import compute_data
+
+
+with open("config.json") as f:
+    config=json.load(f)
+data=load_data("GDP-Data.csv")
+cleaned=clean_data(data)
+filtered=filter_data(cleaned,config)
+computation=compute_data(filtered,config)
+print("Computation results:", computation)
