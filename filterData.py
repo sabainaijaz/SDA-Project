1b99a249ccd0c8a0e24b4c8f2036fdda64692e8c
sabainaijaz <sabainaijaz@gmail.com>
2026-02-08 17:13:21 +0500

    updated filter to handle more than one region

--- filterData.py
+++ filterData.py
@@ -1,10 +1,17 @@
 def filter_data(cleaned_data, config):
-    region = config.get("region", "").strip().lower()
+    region = config.get("region", [])
+    if isinstance(region, str):
+        regions = list(map(lambda r: r.strip().lower(), filter(None, region.split(","))))
+    elif isinstance(region, list):
+        regions = list(map(lambda r: r.strip().lower(), region))
+    else: 
+        regions = []
+
     country = config.get("country", "").strip().lower()
     year = config.get("year")
 
     return list(filter(lambda r:
-        (not region or r["Region"].lower() == region) and
+        (not regions or r["Region"].lower() in regions) and
         (not country or r["Country Name"].lower() == country) and
         (not year or r["Year"] == year)
     , cleaned_data))
